48ff4843f3549e2bc68f0dba5448a060085521ff
Nitish Singh <nsingh07@syr.edu>
2020-07-06 11:10:32 -0400

    Added Customer patch

---
+++ src/test/java/com/nitish/springrestapi/services/CustomerServiceImplIT.java
@@ -0,0 +1,106 @@
+package com.nitish.springrestapi.services;
+
+import com.nitish.springrestapi.api.v1.mapper.CustomerMapper;
+import com.nitish.springrestapi.api.v1.model.CustomerDTO;
+import com.nitish.springrestapi.bootstrap.Bootstrap;
+import com.nitish.springrestapi.domain.Customer;
+import com.nitish.springrestapi.repositories.CategoryRepository;
+import com.nitish.springrestapi.repositories.CustomerRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.util.List;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.IsEqual.equalTo;
+import static org.hamcrest.core.IsNot.not;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@ExtendWith(MockitoExtension.class)
+@DataJpaTest
+public class CustomerServiceImplIT {
+
+    @Autowired
+    CustomerRepository customerRepository;
+
+    @Autowired
+    CategoryRepository categoryRepository;
+
+    CustomerService customerService;
+
+    @BeforeEach
+    public void setUp() throws Exception {
+
+        System.out.println("Loading Customer Data");
+        System.out.println(customerRepository.findAll().size());
+
+        //setup data for testing
+        Bootstrap bootstrap = new Bootstrap(categoryRepository, customerRepository);
+        bootstrap.run(); //load data
+
+        customerService = new CustomerServiceImpl(customerRepository, CustomerMapper.INSTANCE);
+    }
+
+    private Long getCustomerIdValue(){
+        List<Customer> customers = customerRepository.findAll();
+
+        System.out.println("Customers Found: " + customers.size());
+
+        //return first id
+        return customers.get(0).getId();
+    }
+
+    @Test
+    public void patchCustomerUpdateFirstName() throws Exception {
+        String updatedName = "UpdatedName";
+        long id = getCustomerIdValue();
+
+        Customer originalCustomer = customerRepository.getOne(id);
+        assertNotNull(originalCustomer);
+        //save original first name
+        String originalFirstName = originalCustomer.getFirstName();
+        String originalLastName = originalCustomer.getLastName();
+
+        CustomerDTO customerDTO = new CustomerDTO();
+        customerDTO.setFirstName(updatedName);
+
+        customerService.patchCustomer(id, customerDTO);
+
+        Customer updatedCustomer = customerRepository.findById(id).get();
+
+        assertNotNull(updatedCustomer);
+        assertEquals(updatedName, updatedCustomer.getFirstName());
+        assertThat(originalFirstName, not(equalTo(updatedCustomer.getFirstName())));
+        assertThat(originalLastName, equalTo(updatedCustomer.getLastName()));
+    }
+
+    @Test
+    public void patchCustomerUpdateLastName() throws Exception {
+        String updatedName = "UpdatedName";
+        long id = getCustomerIdValue();
+
+        Customer originalCustomer = customerRepository.getOne(id);
+        assertNotNull(originalCustomer);
+
+        //save original first/last name
+        String originalFirstName = originalCustomer.getFirstName();
+        String originalLastName = originalCustomer.getLastName();
+
+        CustomerDTO customerDTO = new CustomerDTO();
+        customerDTO.setLastName(updatedName);
+
+        customerService.patchCustomer(id, customerDTO);
+
+        Customer updatedCustomer = customerRepository.findById(id).get();
+
+        assertNotNull(updatedCustomer);
+        assertEquals(updatedName, updatedCustomer.getLastName());
+        assertThat(originalFirstName, equalTo(updatedCustomer.getFirstName()));
+        assertThat(originalLastName, not(equalTo(updatedCustomer.getLastName())));
+    }
+}
